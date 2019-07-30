7769ff6b6e08ea50e1b84295db48966012cd7fbb
Pradeep Reddy Raamana <raamana@gmail.com>
2019-07-30 09:41:38 -0400

    locking it down to python 3 or higher only

--- missingdata/__init__.py
+++ missingdata/__init__.py
@@ -5,5 +5,9 @@
 __author__ = """Pradeep Reddy Raamana"""
 __email__ = 'raamana@gmail.com'
 
+from sys import version_info
+if version_info.major < 3:
+    raise NotImplementedError('missingdata package is only tested and supported for '
+                              'Python 3 or higher. We encourage upgrading!')
 
 from missingdata.base import blackholes
